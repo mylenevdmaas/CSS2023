8007a4eb4f32f957bb48251b505034316d891472
mylenevdmaas <55050591+mylenevdmaas@users.noreply.github.com>
2023-06-20 11:28:15 +0200

    Framework

---
+++ mcmc.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+
+class MCMC():
+    def __init__(self, file):
+        self.J = # read file
+
+    def run_simulation(self, n_sim, T):
+
+        # create empty dataframe
+
+        # create random config
+
+        # calculate measures?
+
+        for i in range(n_sim):
+
+            # flip random spin
+
+            # calculate energy difference
+
+            # accept flip based on energy diff and boltzman
+
+            # calculate new measures
+
+            # save to dataframe
+
+        # return dataframe or save dataframe to csv
