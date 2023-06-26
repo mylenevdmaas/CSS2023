ee725aa64acbc0d0415c0cba8c2138419ffb6a15
conorgouraud <60387223+conorgouraud@users.noreply.github.com>
2023-06-26 09:40:07 +0200

    run sim conor

--- mcmc_conor.py
+++ mcmc_conor.py
@@ -2,6 +2,17 @@ import numpy as np
 from numba import njit
 import matplotlib.pyplot as plt
 import random
+import csv
+import pandas as pd
+
+
+with open('DTI_avg_log.csv', 'r') as file:
+    reader = csv.reader(file, delimiter=',')
+    data = list(reader)
+DTI_log = pd.DataFrame(data, dtype = 'float64')
+DTI= DTI_log.values
+n = 90
+
 
 @njit
 def conn_matrix_basic(n):
@@ -13,7 +24,7 @@ def conn_matrix_basic(n):
 #@njit
 def conn_matrix_not_so_basic(n, fraction_of_zeros):
     """Returns nxn symmatric matrix for J with random numbers in [0,1]."""
-    J_tri = np.tril(np.random.uniform(1, 1, size=(n, n)), -1)
+    J_tri = np.tril(np.random.uniform(0, 1, size=(n, n)), -1)
     J = np.zeros((n,n)) + J_tri + J_tri.T
 
     f = int(np.floor(fraction_of_zeros*n*(n-1)/2))
@@ -74,7 +85,7 @@ def metropolis(spins, n_iterations, T, c_matrix):
         spins_timeseries[i] = spins
 
     # discard burn-in period and get mean of magnetization
-    magnetisation_list = magnetisation_list[500:]
+    magnetisation_list = magnetisation_list[1000:]
     avg_magnetisation = np.mean(magnetisation_list) 
     
     # calculate susceptibility
@@ -88,14 +99,15 @@ def metropolis(spins, n_iterations, T, c_matrix):
     return spins, avg_magnetisation, susceptibility, model_corr, last_spins
 
 #@njit
-def multi_metropolis(n_simulations, n_iterations, T, n, fraction_zeros):
+def multi_metropolis(n_simulations, n_iterations, T, n, fraction_zeros, input_matrix):
     """Runs n_simulations runs of the metropolis algorithm."""
 
     list_avg_magnetisation = np.zeros(n_simulations)
     list_sus = np.zeros(n_simulations)
     list_model_corr = []
 
-    c_matrix = conn_matrix_basic(n)
+    #c_matrix = conn_matrix_basic(n)
+    c_matrix = input_matrix 
 
     for i in range(n_simulations):
 
@@ -117,10 +129,10 @@ def multi_metropolis(n_simulations, n_iterations, T, n, fraction_zeros):
     
     mean_corr_matrix = np.mean(list_model_corr, axis=0)
 
-    return mean_magnet, std_magnet, mean_sus, std_sus, mean_corr_matrix
+    return mean_magnet, std_magnet, mean_sus, std_sus, mean_corr_matrix, list_model_corr
     
 #@njit
-def run_simulation(n_simulations:int, n_iterations:int, T_list:np.array, n:int, fraction_zeros):
+def run_simulation(n_simulations:int, n_iterations:int, T_list:np.array, n:int, fraction_zeros, input_matrix):
     """Runs metropolis simulations for every temperature in a list of temperatures."""
     n_temp = len(T_list)
 
@@ -129,7 +141,7 @@ def run_simulation(n_simulations:int, n_iterations:int, T_list:np.array, n:int, 
     means_sus = np.zeros(n_temp)
     stds_sus = np.zeros(n_temp)
     for i, T in enumerate(T_list):
-        means_mag[i], stds_mag[i], means_sus[i], stds_sus[i], _ = multi_metropolis(n_simulations, n_iterations, T, n, fraction_zeros)
+        means_mag[i], stds_mag[i], means_sus[i], stds_sus[i], _, _ = multi_metropolis(n_simulations, n_iterations, T, n, fraction_zeros, input_matrix)
 
     return [means_mag, stds_mag, means_sus, stds_sus]
 
